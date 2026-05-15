dd5320408deba2c1c8d7e66d3bab419c9638e681
iliryummy <pleshinailirjam@gmail.com>
2026-05-16 00:00:09 +0200

    update generate_keys

---
+++ generate_keys.py
@@ -0,0 +1,30 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_keys():
+    print("Duke gjeneruar celesat RSA...")
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    
+    
+    with open("private_key.pem", "wb") as f:
+        f.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
+    
+    
+    public_key = private_key.public_key()
+    with open("public_key.pem", "wb") as f:
+        f.write(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+    print("Celesat u gjeneruan: private_key.pem dhe public_key.pem")
+
+if __name__ == "__main__":
+    generate_keys()
